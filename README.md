3e9fc1f73e18af603ab02c4a22d0e165be57d320
GerhardYang <gh.yang@outlook.com>
2018-12-17 21:37:45 +0800

    本地化依赖

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-#SuperMap_Cesium_Demo
+<h3 id="example-1">目录结构</h3>
+
+范例目录结构：
+``` 
+            ./
+              └─3dwebgl         3dwebgl范例目录
+                  ├─img             缩略图目录
+                  └─config.js       配置文件  
+              ├─Cesium       SuperMap Cesium库
+              ├─css         公共css目录
+              ├─fonts       公共字体目录
+              ├─img         公共图片目录
+              ├─js          公共js目录
+              ├─libs        公共依赖
+              ├─locales     语言包
+              │─template    界面模板
+              
+  
+  
+```
+
+<h3 id="example-5">配置文件</h3> 
+    
+ 每个客户端范例目录(如：`/example/leaflet`)下都有一个`config.js`文件，文件在站点初始化时读取。
+ 
+ `config.js`中范例相关的配置项为`exampleConfig`，参数如下：
+ 
+ 左边为侧边栏菜单，右边为示例列表
+ 
+ ![config](./config.png)
+
+<h3 id="example-7">注意事项</h3>
+
+暂无
+
+                               
 
 
-Gerhard'S SuperMap_Cesium Demo
+  
+
--- js/include-web.js
+++ js/include-web.js
@@ -36,16 +36,15 @@
     //加载类库资源文件
     function load() {
         var includes = (targetScript.getAttribute('include') || "").split(",");
-        var excludes = (targetScript.getAttribute('exclude') || "").split(",");
 
         var jQueryInclude = false;
 
         if (inArray(includes, 'jquery') && !jQueryInclude) {
-            inputScript("../libs/jquery3.2.1/jquery.min.js");
+            inputScript("../libs/jquery/jquery.3.2.1.min.js");
         }
 
         if (inArray(includes, 'bootstrap')) {
-            inputScript("../libs/jquery3.2.1/jquery.min.js");
+            inputScript("../libs/jquery/jquery.3.2.1.min.js");
             inputCSS("../libs/bootstrap/bootstrap.min.css");
             inputScript("../libs/bootstrap/bootstrap.min.js");
         }
@@ -55,30 +54,28 @@
         }
 
         if (inArray(includes, 'jquery.scrollto')) {
-            inputScript("../libs/jquery.scrollto/jquery.scrollTo.min.js");
+            inputScript("../libs/jquery/jquery.scrollTo.min.js");
         }
         if (inArray(includes, 'ace')) {
-            inputScript("https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.6/ace.js");
+            inputScript("../libs/ace/ace.js");
         }
 
         if (inArray(includes, 'admin-lte')) {
-            inputCSS("http://iclient.supermap.io/libs/admin-lte/css/AdminLTE.min.css");
-            inputCSS("http://iclient.supermap.io/libs/admin-lte/css/skins/skin-blue.min.css");
-            inputCSS("http://iclient.supermap.io/libs/font-awesome/css/font-awesome.min.css");
-            inputScript("http://iclient.supermap.io/libs/admin-lte/js/app.min.js");
+            inputCSS("../libs/admin-lte/AdminLTE.min.css");
+            inputCSS("../libs/admin-lte/skin-blue.min.css");
+            inputCSS("../libs/font-awesome/css/font-awesome.min.css");
+            inputScript("../libs/admin-lte/app.min.js");
         }
         if (inArray(includes, 'lazyload')) {
-            inputScript("https://cdnjs.cloudflare.com/ajax/libs/jquery_lazyload/1.9.7/jquery.lazyload.min.js");
+            inputScript("../libs/jquery/jquery.lazyload.min.js");
         }
         if (inArray(includes, 'i18n')) {
-            inputScript("https://cdnjs.cloudflare.com/ajax/libs/i18next/10.0.7/i18next.min.js");
-            inputScript("https://cdnjs.cloudflare.com/ajax/libs/jquery-i18next/1.2.1/jquery-i18next.min.js");
+            inputScript("../libs/i18next/i18next.min.js");
+            inputScript("../libs/jquery/jquery-i18next.min.js");
         }
     }
 
     load();
-    window.isLocal = false;
-    window.server = document.location.toString().match(/file:\/\//) ? "http://localhost:8090" : document.location.protocol + "//" + document.location.host;
-    window.version = "9.1.0";
-    window.preRelease = "";
+
+    window.version = "1.0";
 })();
